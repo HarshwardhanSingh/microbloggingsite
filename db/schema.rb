ccc8b5443134b1eede08ef157a23a5f39fd08dda
Bhanwarsa <harsh.rathore14@gmail.com>
2016-03-28 22:58:47 +0530

    Paperclip gem config

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160328170121) do
+ActiveRecord::Schema.define(version: 20160328172701) do
 
   create_table "replies", force: :cascade do |t|
     t.text     "content"
@@ -23,9 +23,13 @@ ActiveRecord::Schema.define(version: 20160328170121) do
 
   create_table "tweets", force: :cascade do |t|
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,6 +46,10 @@ ActiveRecord::Schema.define(version: 20160328170121) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "username"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
