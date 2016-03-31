1824ba1b3c6a25620f68c71ef291c6c723bdf051
Bhanwarsa <harsh.rathore14@gmail.com>
2016-03-31 22:35:39 +0530

    Added functionality to tweet controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,19 @@ class PagesController < ApplicationController
 
   def welcome
   end
+
+  def dashboard
+  end
+
+  def followers
+  end
+
+  def followings
+  end
+
+  def userpage
+  end
+
   
+
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,48 @@
 class TweetsController < ApplicationController
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user_id = current_user.id
+    @tweet.save
+    respond_to do |format|
+      format.html{ redirect_to tweet_path(@tweet) }
+      format.js{}
+    end
+  end
+
+  def show
+    @tweet = Tweet.find(params[:tweet_id])
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:tweet_id])
+  end
+
+  def update
+    @tweet = Tweet.find(params[:tweet_id])
+    @tweet.update(tweet_params)
+    @tweet.save
+    respond_to do |format|
+      format.html{ redirect_to tweet_path(@tweet) }
+      format.js{}
+    end
+  end
+
+  def destroy
+    @tweet = Tweet.find(params[:tweet_id])
+    @tweet.destroy
+    respond_to do |format|
+      format.html{ redirect_to :back }
+      format.js{}
+    end
+  end
+
+  private
+    def tweet_params
+      params.require(:tweet).permit(:content)
+    end
 end
