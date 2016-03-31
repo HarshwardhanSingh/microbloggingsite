d5f58f1ce83e7fba8ae48491f07f346fdffabd2e
Bhanwarsa <harsh.rathore14@gmail.com>
2016-03-31 19:10:36 +0530

    Added functionalities to reply controller

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,2 +1,25 @@
 class RepliesController < ApplicationController
+
+  def create
+    @tweet = Tweet.find(params[:tweet_id])
+    @reply = Reply.new(reply_params)
+    @reply.user_id = current_user.id
+    @reply.tweet_id = @tweet.tweet_id
+    @reply.save
+    respond_to do |format|
+      format.html{redirect_to tweet_path(@tweet)}
+      format.js{}
+    end
+  end
+
+  def destroy
+    @tweet = Tweet.find(params[:tweet_id])
+    @tweet.destroy
+    respond_to do |format|
+      format.html{ redirect_to :back }
+      format.js{}
+    end
+  end
+
+
 end
