b05696b7631a410da6a83b4d70ce1b9d854f96ba
Bhanwarsa <harsh.rathore14@gmail.com>
2016-04-03 22:30:05 +0530

    Added signin with username functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,31 @@
 class User < ActiveRecord::Base
+  attr_accessor :login
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-   has_attached_file :avatar, styles: { medium: "300x300#", thumb: "40x40#" }, default_url: "/images/:style/missing.png"
-   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+	has_attached_file :avatar, styles: { medium: "300x300#", thumb: "40x40#" }, default_url: "/images/:style/missing.png"
+	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
-   has_many :tweets
-   has_many :replies
+	has_many :tweets
+	has_many :replies
+
+    def self.find_for_database_authentication(warden_conditions)
+		conditions = warden_conditions.dup
+		if login = conditions.delete(:login)
+			where(conditions).where(["lower(username) = lower(:value) OR lower(email) = lower(:value)", { :value => login }]).first
+		elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+			where(conditions.to_hash).first
+		end
+	end
+
+	validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+
+	validates :username,
+		:presence => true,
+		:uniqueness => {
+		:case_sensitive => false
+		} # etc.
 end
